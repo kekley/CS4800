e7bcfe780bb68d64ada6185c368df149c004525d
kekley <jesusmacias8568@gmail.com>
2026-03-20 22:23:27 -0700

    delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello():
-    print(url_for("static", filename="index.html"))
-    return "<h1>Hello, World!</h1>"
